abfa9c6c383593a58012b426299ff25b9f460722
Whaoo <louis.gid13@gmail.com>
2024-09-29 21:47:31 +0200

    French

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const updateCountdown = () => {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    countdown.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    countdown.innerHTML = `${days}j ${hours}h ${minutes}m ${seconds}s`;
 
     if (distance < 0) {
         clearInterval(interval);
