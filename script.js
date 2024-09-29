ad265444d1e625c999aa1093dd54fac78c642e27
Whaoo <louis.gid13@gmail.com>
2024-09-29 21:01:41 +0200

    firstcom

---
+++ script.js
@@ -0,0 +1,22 @@
+const targetDate = new Date('2025-01-30T23:59:59').getTime();
+
+const countdown = document.getElementById('countdown');
+
+const updateCountdown = () => {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    countdown.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+    if (distance < 0) {
+        clearInterval(interval);
+        countdown.innerHTML = "EXPIRED";
+    }
+};
+
+const interval = setInterval(updateCountdown, 1000);
